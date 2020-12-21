8ccb3fe24812c4c72f74073c38ee889520bf3dde
sogafish <k.sogafish@gmail.com>
2020-12-22 00:00:15 +0900

    :sparkles: Create Counter

--- src/App.svelte
+++ src/App.svelte
@@ -1,30 +1,21 @@
 <script lang="ts">
-  export let name: string;
+  import Counter from './components/Counter.svelte';
+  import Button from './components/Button.svelte';
+
+  let count = 0;
+  const incrementCount = () => {
+    count += 1;
+  };
 </script>
 
-<main>
-<h1>Hello {name}!</h1>
-  <p>Visit the <a href="https://svelte.dev/tutorial">Svelte tutorial</a> to learn how to build Svelte apps.</p>
-</main>
+<div>
+  <Counter
+    count={count}
+  />
+  <Button
+    onClick={incrementCount} text='OnClick'
+  />
+</div>
 
 <style>
-  main {
-    text-align: center;
-    padding: 1em;
-    max-width: 240px;
-    margin: 0 auto;
-  }
-
-  h1 {
-    color: #ff3e00;
-    text-transform: uppercase;
-    font-size: 4em;
-    font-weight: 100;
-  }
-
-  @media (min-width: 640px) {
-    main {
-      max-width: none;
-    }
-  }
 </style>
---
+++ src/components/Button.svelte
@@ -0,0 +1,11 @@
+<script lang="ts">
+  let onClick: () => void;
+  let text: string = 'Click';
+
+  export {
+    onClick,
+    text,
+  };
+</script>
+
+<button on:click={onClick}>{text}</button>
