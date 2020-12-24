59eec84a36366a48b7493a144ee00df4ac36497a
sogafish <k.sogafish@gmail.com>
2020-12-24 10:53:28 +0900

    :sparkles: Create Header

---
+++ src/components/Header.svelte
@@ -0,0 +1,22 @@
+<script lang="ts">
+</script>
+
+<div class="header">
+  <p class="header-title">Header</p>
+</div>
+
+<style>
+  .header {
+    position: fixed;
+    height: 50px;
+    width: 100%;
+    background-color: #008080;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+  }
+  .header-title {
+    font-size: 20px;
+    color: #fff8;
+  }
+</style>
